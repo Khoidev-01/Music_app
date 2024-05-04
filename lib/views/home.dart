ce0f7207d96dcac7db454c9e49433cbca4b4dcd4
Khoidev-01 <trandinhkhoi.dx@gmail.com>
2024-05-04 08:24:58 +0700

    init project

---
+++ lib/views/home.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_player_app/consts/colors.dart';
+import 'package:music_player_app/consts/text_style.dart';
+import 'package:music_player_app/controllers/player_controller.dart';
+import 'package:music_player_app/views/player.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.put(PlayerController());
+    return Scaffold(
+      backgroundColor: bgDarkColor,
+      appBar: AppBar(
+        backgroundColor: bgDarkColor,
+        actions: [
+          IconButton(
+            onPressed: () {
+              NavigationBar(
+                backgroundColor: Colors.amber,
+                destinations: const [],
+              );
+            },
+            icon: const Icon(
+              Icons.search,
+              color: whiteColor,
+              size: 30,
+            ),
+          ),
+        ],
+        leading: const Icon(
+          Icons.sort_rounded,
+          color: whiteColor,
+          size: 30,
+        ),
+        title: Text(
+          "MP3",
+          style: ourStyle(
+            family: bold,
+            size: 18,
+          ),
+        ),
+      ),
+      body: FutureBuilder<List<SongModel>>(
+        future: controller.audioQuery.querySongs(
+          ignoreCase: true,
+          orderType: OrderType.ASC_OR_SMALLER,
+          sortType: null,
+          uriType: UriType.EXTERNAL,
+        ),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.data!.isEmpty) {
+            if (kDebugMode) {
+              print(snapshot.data);
+            }
+            return Center(
+              child: Text(
+                'No song found',
+                style: ourStyle(),
+              ),
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 4),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      tileColor: bgColor,
+                      title: Text(
+                        snapshot.data![index].displayNameWOExt,
+                        style: ourStyle(
+                          family: bold,
+                          size: 15,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "${snapshot.data![index].artist}",
+                        style: ourStyle(
+                          family: regular,
+                          size: 12,
+                        ),
+                      ),
+                      leading: QueryArtworkWidget(
+                        id: snapshot.data![index].id,
+                        type: ArtworkType.AUDIO,
+                        nullArtworkWidget: const Icon(
+                          Icons.music_note,
+                          color: whiteColor,
+                          size: 32,
+                        ),
+                      ),
+                      trailing: controller.playIndex.value == index &&
+                              controller.isPlaying.value
+                          ? const Icon(Icons.play_arrow,
+                              color: whiteColor, size: 26)
+                          : null,
+                      onTap: () {
+                        Get.to(
+                            () => Player(
+                                  data: snapshot.data!,
+                                ),
+                            transition: Transition.downToUp);
+                        controller.playsong(snapshot.data![index].uri, index);
+                      },
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
